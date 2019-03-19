5088ef03e1d34d18f253ae6213f16d0311037477
Jalil <Jalil@LAPTOP-T3IK0MSI>
2019-03-19 23:13:06 +0530

    modified

--- C/Assignment 4/15re/15re.cpp
+++ C/Assignment 4/15re/15re.cpp
@@ -1,119 +1,151 @@
 #include "pch.h"
 #include<stdio.h>
 #include<stdlib.h>
-#include<iostream>
-#define MAX 100
+#define EXCEPTION 1
+#define MAX_LENGTH 50
 using namespace std;
 
-int strlen1(char *s)
+int strlen(char *string1)
 {
-	int i = 0, len = 0;
-	for (i = 0; s[i] != '\0'; i++)
+	int length = 0;
+	while (*(string1 + length) != '\0')
 	{
-		len++;
+		length++;
 	}
-	return len;
+	return length;
+}
+void strcpy(char *string2, char *string1)
+{
+	int i = 0;
+	while (*(string1 + i) != '\0')
+	{
+		*(string2 + i) = *(string1 + i);
+		i++;
+	}
+	*(string2 + i) = '\0';
 }
 int strcmp(char *string1, char *string2)
 {
-	int i = 0, minlen = 0;
-	if (strlen1(string1) < strlen(string2))
+	int i = 0, j = 0;
+	char string1char = *(string1 + i), string2char = *(string2 + j);
+	while (string1char == string2char)
 	{
-		minlen = strlen1(string1);
+		i++;j++;
+		if (i >= strlen(string1))
+			break;
+		if (j >= strlen(string2))
+			break;
+		string1char = *(string1 + i);
+		string2char = *(string2 + j);
 	}
-	else minlen = strlen1(string2);
-	for (i = 0; i < minlen; i++)
-	{
-		if (*(string1 + i) < *(string2 + i))
-		{
-			return -1;
-		}
-		else if (*(string1 + i) > *(string2 + i))
-		{
-			return 1;
-		}
-	}
-	if (strlen1(string1) == strlen1(string2))
-	{
-		return 0;
-	}
-	if (strlen1(string1) > strlen(string2))
+	if (string1char > string2char)
 	{
 		return 1;
 	}
-	return -1;
-}
-char* strrev1(char *s)
-{
-	int i = 0, j = 0; char t;
-	for (i = 0; s[i] != '\0'; i++);
-	i--;
-	for (; j < i; j++, i--)
+	else if (string2char > string1char)
 	{
-		t = *(s + i); *(s + i) = *(s + j); *(s + j) = t;
+		return -1;
 	}
-	return s;
+	if (*(string1 + i) == '\0' && *(string2 + j) == '\0')
+		return 0;
+	else if (*(string1 + i) == '\0')
+		return -1;
+	else if (*(string2 + j) == '\0')
+		return 1;
 }
-char* strcat1(char *s1, char *s2)
+void strcat(char *string1, char *string2)
 {
-	int i = 0, j = 0;
-	for (i = 0; s1[i] != '\0'; i++);
-	for (; s2[j] != '\0'; j++, i++)
+	int length1 = strlen(string1);
+	int length2 = strlen(string2);
+	if (length1 + length2 > MAX_LENGTH)
 	{
-		s1[i] = s2[j];
+		printf("Array index out of bounds\n");
+		system("pause");
+		exit(1);
 	}
-	s1[i] = '\0';
-	return s1;
+	int j = length1;
+	for (int i = 0; i < length2; i++, j++)
+	{
+		*(string1 + j) = *(string2 + i);
+	}
+	*(string1 + j) = '\0';
 }
-char * strcpy1(char* s1, char* s2)
+void strrev(char *string1)
 {
-	int i = 0;
-	for (i = 0; s2[i] != '\0'; i++)
+	int length = strlen(string1);
+	char *string2 = (char *)calloc(length + 1, sizeof(char));
+	int i = length - 1;
+	for (int j = 0; i >= 0; i--, j++)
 	{
-		s1[i] = s2[i];
+		*(string2 + i) = *(string1 + j);
 	}
-	s1[i] = '\0';
-	return s1;
+	*(string2 + length) = '\0';
+	strcpy(string1, string2);
 }
+
 int main()
 {
-	int len1 = 0, len2 = 0;
-	char *string1 = (char *)malloc(MAX * sizeof(char));
-	char *string2 = (char *)malloc(MAX * sizeof(char));
-	char dup[2];
-	cout << "Enter length of string 1" << endl;
-	cin >> len1;
-	if (len1 > MAX)
+	int length1 = -1, length2 = -1;
+	char string1[MAX_LENGTH], string1copy[MAX_LENGTH], string2[MAX_LENGTH], enter[2];
+	printf("Enter size of the string : ");
+	scanf("%d", &length1);
+	if (length1 >= MAX_LENGTH)
 	{
-		cout << "Entered invalid length" << endl;
-		return 0;
+		printf("Cannot have length more than %d\n", MAX_LENGTH - 1);
+		system("pause");
+		exit(1);
 	}
-	gets_s(dup, 2);
-	cout << "Enter string 1" << endl;
-	fgets(string1, len1+1, stdin);
-	cout << "Enter length of string 2" << endl;
-	cin >> len2;
-	if (len2 > MAX)
+	gets_s(enter, 1);
+	printf("Enter string1: ");
+	try
 	{
-		cout << "Entered invalid length" << endl;
-		return 0;
+		gets_s(string1, MAX_LENGTH);
+		if (strlen(string1) > length1)
+			throw EXCEPTION;
 	}
-	gets_s(dup, 2);
-	cout << "Enter string 2" << endl;
-	fgets(string2, len2+1, stdin);
-	cout << "Length of string 1: " << strlen1(string1) << endl;
-	cout << "Length of string 2: " << strlen1(string2) << endl;
-	cout << "string1: " << string1 << endl;
-	cout << "string2: " << string2 << endl;
-	cout << "strcmp: " << strcmp(string1, string2) << endl;
-	cout << "Reversing string1" << endl;
-	strrev1(string1);
-	cout << "string1: " << string1 << endl;
-	cout << "concatenating string1 with string2" << endl;
-	strcat1(string1, string2);
-	cout << "string1: " << string1 << endl;
-	cout << "copying string2 to string1" << endl;
-	strcpy1(string1, string2);
-	cout << "string1: " << string1 << endl;
-	free(string1); free(string2);
+	catch (int exception)
+	{
+		printf("Array size exception\n");
+		system("pause");
+		exit(1);
+	}
+	printf("String entered is %s\n", string1);
+	printf("Performing strcpy\n");
+	strcpy(string1copy, string1);
+	printf("Copy of string1 is %s\n", string1copy);
+	printf("Enter size of string2:");
+	scanf("%d", &length2);
+	if (length2 >= MAX_LENGTH)
+	{
+		printf("Cannot have length more than %d\n", MAX_LENGTH-1);
+		system("pause");
+		exit(1);
+	}
+	gets_s(enter, 1);
+	printf("Enter string2:");
+	try
+	{
+		gets_s(string2, MAX_LENGTH);
+		if (strlen(string2) > length2)
+			throw EXCEPTION;
+	}
+	catch (int exception)
+	{
+		printf("Array size exception\n");
+		system("pause");
+		exit(1);
+	}
+	printf("String entered is %s\n", string2);
+	int str_diff = strcmp(string1, string2);
+	printf("strcmp(%s,%s)=%d\n", string1, string2, str_diff);
+	int str1len = strlen(string1);
+	printf("Length of %s: %d\n", string1, str1len);
+	printf("concatenation of string1 and string2:");
+	strcat(string1, string2);
+	printf(" %s\n", string1);
+	printf("reverse of string1:");
+	strrev(string1);
+	printf(" %s\n", string1);
+	system("pause");
+	return 0;
 }
